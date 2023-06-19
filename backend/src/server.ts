b8568ea2e444931b1fa30f45970fa476f635f8c8
Zai <zaiyellyintaung@gmail.com>
2023-06-19 16:20:58 +0630

    Done backend setup

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
