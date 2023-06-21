c19f1955b5493991bbdd509c81efab4d257c3cac
Zai <zaiyellyintaung@gmail.com>
2023-06-21 08:22:59 +0630

    Before changing controller

--- backend/src/auth/User.ts
+++ backend/src/auth/User.ts
@@ -5,7 +5,6 @@ import { UserType } from "../types";
 const userSchema: Schema = new Schema<UserType>({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  seller: { type: Boolean, default: false },
   profile: { type: Schema.Types.ObjectId, ref: "Profile" },
   created: { type: Date, default: Date.now },
 });
