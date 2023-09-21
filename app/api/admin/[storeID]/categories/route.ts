0b223b01b4700bb83e0ec621723c381bb2693ad4
Zai <zaiyellyintaung@gmail.com>
2023-09-22 05:36:18 +0630

    starting api routes for post

---
+++ app/api/admin/[storeID]/categories/route.ts
@@ -0,0 +1,53 @@
+import { useParams } from "next/navigation"
+import { NextResponse } from "next/server"
+import { auth } from "@clerk/nextjs"
+
+import prismadb from "@/lib/prismadb"
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeID: string } }
+) {
+  try {
+    const { userId } = auth()
+    const body = await req.json()
+    const { name, billboardID } = body
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 403 })
+    }
+
+    if (!name || !billboardID) {
+      return new NextResponse("name,billboardID is required", {
+        status: 400,
+      })
+    }
+
+    if (!params.storeID) {
+      return new NextResponse("Store id is required", { status: 400 })
+    }
+
+    const storeByUserID = await prismadb.store.findFirst({
+      where: {
+        id: params.storeID,
+        userId,
+      },
+    })
+
+    if (!storeByUserID) {
+      return new NextResponse("Unauthorized", { status: 405 })
+    }
+    const newItem = await prismadb.category.create({
+      data: {
+        name,
+        billboardID,
+        storeID: params.storeID,
+      },
+    })
+
+    return NextResponse.json(newItem)
+  } catch (error) {
+    console.log("[CATEGORY_POST]", error)
+    return new NextResponse("Internal error", { status: 500 })
+  }
+}
