8ec8ef4110bce6bb0c442fad0c921515cfefd01d
Zai <zaiyellyintaung@gmail.com>
2023-10-07 23:40:00 +0630

    revising edit feature and api

--- app/(admin)/admin/[storeID]/billboards/[billboardID]/page.tsx
+++ app/(admin)/admin/[storeID]/billboards/[billboardID]/page.tsx
@@ -1,10 +1,18 @@
+import prismadb from "@/lib/prismadb"
 import BillboardForm from "../_components/BillboardForm"
 
-export default function BillBoardDetailPage({
+export default async function BillBoardDetailPage({
   params,
 }: {
   params: { billboardID: string }
 }) {
+
+  const billboard = await prismadb.billboard.findUnique({
+    where:{
+      id:params.billboardID
+    }
+  })
+
   return (
     <main className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
